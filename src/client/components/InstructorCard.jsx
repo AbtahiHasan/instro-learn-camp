c20a9e41b6986cbce8d04ac7bd3e62b1b75d0ff0
AbtahiHasan <anikidpersonal@gmail.com>
2023-06-10 15:59:51 +0600

    animated added

--- src/client/components/InstructorCard.jsx
+++ src/client/components/InstructorCard.jsx
@@ -1,16 +1,28 @@
+import {motion} from "framer-motion"
+
 
 
 const InstructorCard = ({instructor}) => {
     const {name, image, email} = instructor
     return (
-        <article data-aos="zoom-in" className="text-center border-2 shadow-md rounded p-4">           
-            <div className="relative">
-                <img className="w-full " src={image} alt="" />
+        <motion.article 
+            initial={{opacity: 0, scale: 0.8}}
+            animate={{ opacity: 1, scale: 1}}
+            transition={{type: 'spring', stiffness: 120}}
+         className="text-center border-2 shadow-md rounded p-4">           
+            <div className="relative overflow-hidden h-[200px]">
+                <motion.img 
+                initial={{opacity: 0, scale: 0}}
+                animate={{opacity: 1, scale: 1}}
+                whileHover={{scale: 1.1}}
+                transition={{type: "spring", stiffness: 120}}
+                
+                className="w-full" src={image} alt="" />
                 <div className="bg-main absolute bottom-0 h-[150px] w-full -z-10"></div>
             </div>
             <h3 className="font-bold text-2xl my-2">{name}</h3>
             <p>{email}</p>
-        </article>
+        </motion.article>
     );
 };
 
